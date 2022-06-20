3f71041cef2af59abdc8bd3926733c3663050aad
ThePhantomDev <fordjourbobiem@gmail.com>
2022-06-20 18:37:39 +0000

    migrations to db

---
+++ backend/config/tableUtil.config.js
@@ -0,0 +1,55 @@
+export function createTableName(knex, tableName) {
+	return knex.schema.createTable(tableName, (table) => {
+		table.uuid(`${tableName}_Id`).notNullable().primary()
+		table.string(`${tableName}_name`).notNullable()
+	})
+}
+export function createTableNameInt(knex, tableName) {
+	return knex.schema.createTable(tableName, (table) => {
+		table.integer(`${tableName}_Id`).notNullable().primary()
+		table.string(`${tableName}_name`).notNullable()
+	})
+}
+
+export function references(
+	table,
+	tableName,
+	notNullable = true,
+	columnName = ''
+) {
+	const definition = table
+		.integer(`${columnName || tableName}_Id`)
+		.unsigned()
+		.references(`${columnName || tableName}_Id`)
+		.inTable(tableName)
+		.onDelete('cascade')
+
+	if (notNullable) {
+		definition.notNullable()
+	}
+	return definition
+}
+export function referencesUUID(
+	table,
+	tableName,
+	notNullable = true,
+	columnName = ''
+) {
+	const definition = table
+		.uuid(`${columnName || tableName}_Id`)
+		.references(`${columnName || tableName}_Id`)
+		.inTable(tableName)
+		.onDelete('cascade')
+
+	if (notNullable) {
+		definition.notNullable()
+	}
+	return definition
+}
+
+export default {
+	createTableName,
+	createTableNameInt,
+	references,
+	referencesUUID,
+}
--- backend/models/book.moodel.js
+++ backend/models/book.moodel.js
@@ -1,5 +1,9 @@
 import { Model } from 'objection'
-import tablesConfig from '../config/tables.config'
+import tablesConfig from '../config/tables.config,js'
+import connection from '../config/db.config'
+
+// initializing the db config
+Model.knex(connection)
 
 class Book extends Model {
 	static get tableName() {
